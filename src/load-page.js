5b630ff6216bb599bf861a88042493fda921676e
t0ntin <115113636+t0ntin@users.noreply.github.com>
2023-10-27 17:26:21 -0500

    Refactored code
    Buttons work correctly. Changed li elements to buttons, made code more semantically accurate.

--- src/load-page.js
+++ src/load-page.js
@@ -2,80 +2,61 @@ import './style.css';
 import plateImage from './plate.png';
 import { renderMenu } from './menu';
 
-function createHeader() {
+function createHeader(main) {
   const header = document.createElement('header');
+  const nav = document.createElement('nav');
   const title = document.createElement('p');
-  title.textContent = 'The Punderful Plate';
+
+  main.append(header);
   header.append(title);
-  header.appendChild(createNav());
-  return header;
+  title.textContent = 'The Punderful Plate';
+  header.append(nav);
+  const sectionNames = ['Home', 'Menu', 'Contact'];
+  for (let i = 0; i < 3; i++) {
+    const button = document.createElement('button');
+    button.innerText = sectionNames[i];
+    nav.append(button);
+  }
 }
 
-function createNav() {
-  const nav = document.createElement('nav');  
-  nav.append(createNavUl());
-  return nav;
+function createSection(main) {
+  const section = document.createElement('section');
+  main.append(section);
+  return section;
 }
-
-function createNavUl() {
-  const navUl = document.createElement('ul');
-  const liElements = createNavLi();
-  liElements.forEach(li => {
-    navUl.appendChild(li); 
-  });
-  return navUl;
-}
-
-function createNavLi() {
-  const navHomeLi = document.createElement('li');
-  navHomeLi.textContent = 'Home';
-
-  const navMenuLi = document.createElement('li');
-  navMenuLi.textContent = 'Menu';
-
-  const navContactLi = document.createElement('li');
-  navContactLi.textContent = 'Contact';
-
-  return [navHomeLi, navMenuLi, navContactLi];
-}
-
-function renderHome(main) {
-  let activePage = {homeIsActive : true};
-  const bodyEl = document.createElement('div');
-  main.append(bodyEl);
-  const plate = new Image();
-  plate.src = plateImage;
-  bodyEl.append(plate);
-  const description = document.createElement('p');
-  bodyEl.append(description);
-  description.innerText = `Welcome to The Punderful Plate. Our quirky and cozy eatery is a delightful haven for food enthusiasts and pun aficionados alike. From our delectable "Punderful Pasta" to our "Hilarious Hamburgers," you will savor the flavor of pun-omenal cuisine. Come for the food, stay for the puns!`
-}
-
 
 function createFooter(main) {
   const footer = document.createElement('footer');
   main.append(footer);
-  footer.textContent = "test";
 }
 
+function renderSection(section) {
+  const plate = new Image();
+  plate.src = plateImage;
+  section.append(plate);
+  const description = document.createElement('p');
+  section.append(description);
+    description.innerText = `Welcome to The Punderful Plate. Our quirky and cozy eatery is a delightful haven for food enthusiasts and pun aficionados alike. From our delectable "Punderful Pasta" to our "Hilarious Hamburgers," you will savor the flavor of pun-omenal cuisine. Come for the food, stay for the puns!`
+}
 
 function handleClick() {
-  const navUl = document.querySelector('ul');
-  navUl.addEventListener('click', function (event) {
-    if (event.target.tagName === 'LI') {
-      console.log('Clicked element:', event.target.innerText);
+  // let activePage = {homeIsActive : true};
+  const nav = document.querySelector('nav');
+  nav.addEventListener('click', function (event) {
+    if (event.target.tagName === 'BUTTON') {
+      console.log("test");
+      
     }
   });
-    
-  }
-
+}
 
 function loadPage() {
   const main = document.querySelector('#content');
-  main.append(createHeader());
-  renderHome(main);
+  
+  createHeader(main);
+  const section = createSection(main);
+  renderSection(section);
   createFooter(main);
-  renderMenu(main);
   handleClick();
 }
 
